d1889d53b863c8dc352478d0246688bf87778907
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-07-30 20:11:12 +0200

    ...

--- hangman-word-game/src/App.jsx
+++ hangman-word-game/src/App.jsx
@@ -4,9 +4,32 @@ import hangmanImages from "./data";
 import data from "./lettersData.js";
 import Letter from "./letter.jsx";
 
+const words = [
+  "BATMAN",
+  "MATRIX",
+  "JOKER",
+  "AVATAR",
+  "GLADIATOR",
+  "INCEPTION",
+  "ALIEN",
+  "FROZEN",
+  "TITANIC",
+  "ROCKY",
+];
+
+const getRandomWord = () => words[Math.floor(Math.random() * words.length)];
+
 function App() {
   const [images, setImage] = useState(hangmanImages);
   const [letters, setLetters] = useState(data);
+  const [wordToGuess, setWordToGuess] = useState(getRandomWord());
+  const [correctGuesses, setCorrectGuesses] = useState([]);
+
+  const handleLetterClick = (letter) => {
+    if (wordToGuess.includes(letter)) {
+      setCorrectGuesses((prev) => [...prev, letter]);
+    }
+  };
 
   return (
     <>
@@ -26,9 +49,21 @@ function App() {
         ))}
       </div>
       <span className="category-span">Guess the MOVIE:</span>
+      <p className="word-display">
+        {wordToGuess
+          .split("")
+          .map((letter) => (correctGuesses.includes(letter) ? letter : "_"))
+          .join(" ")}
+      </p>
+
       <ul className="letters-list">
         {letters.map((letter) => (
-          <Letter key={letter.id} id={letter.id} letter={letter.letter} />
+          <Letter
+            key={letter.id}
+            id={letter.id}
+            letter={letter.letter}
+            onClick={() => handleLetterClick(letter.letter)}
+          />
         ))}
       </ul>
     </>
---
+++ hangman-word-game/src/choseCategory.js
@@ -0,0 +1,27 @@
+const choseCategory = [
+  {
+    id: 1,
+    category: "MOVIE",
+    words: ["BATMAN", "INCEPTION", "GLADIATOR", "AVATAR", "TITANIC"],
+    isActive: false,
+  },
+  {
+    id: 2,
+    title: "TVSHOW",
+    words: ["PRISON BREAK", "LOST", "SHERLOCK", "VIKINGS", "BREAKINGBAD"],
+    isActive: false,
+  },
+  {
+    id: 3,
+    title: "COUNTRY",
+    words: ["CANADA", "BRAZIL", "FRANCE", "SERBIA", "MONTENEGRO"],
+    isActive: false,
+  },
+  {
+    id: 4,
+    title: "ANIMAL",
+    words: ["TIGER", "ELEPHANT", "PENGUIN", "KANGAROO", "GIRAFFE"],
+    isActive: false,
+  },
+];
+export default choseCategory;
